cf4496c317778cd9dc3e78c235084c99242ff27c
bahaashk <bahaa.shk@hotmail.com>
2023-09-20 10:57:36 +0300

    updated pictures sizes

--- src/components/CartProduct.tsx
+++ src/components/CartProduct.tsx
@@ -10,7 +10,7 @@ const CartProduct: React.FC<propsType> = ({img, name, price}) => {
     return (
         <div className=' flex justify-between items-center'>
             <div className=' flex items-center gap-4'>
-                <img className='h-[100px]' src= {img} alt= {name} />
+                <img className='h-[100px] w-[200px] object-cover p-1' src= {img} alt= {name} />
                 <div>
                     <h3 className='font-medium'>{name}</h3>
                     <p className='text-gray-600'> 1 x {price} </p>
